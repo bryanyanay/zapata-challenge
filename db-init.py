7b8982e14fdd4e6de159e59f19ea465c0d35e9e1
Bryan Yan <74737261+bryanyanay@users.noreply.github.com>
2024-03-17 18:44:31 -0400

    Add csv data loading functionality

--- db-init.py
+++ db-init.py
@@ -1,5 +1,6 @@
 import os
 import psycopg2
+import csv
 from psycopg2 import OperationalError
 from dotenv import load_dotenv
 
@@ -27,7 +28,6 @@ def createDB(dbName):
 
 def createTable(dbName):
   try:
-    # Connect to the PostgreSQL database
     conn = psycopg2.connect(
       dbname=dbName,
       user="postgres",
@@ -72,6 +72,38 @@ def createTable(dbName):
     cur.close()
     conn.close()
 
+def loadCSVdata(dbName):
+  try:
+    conn = psycopg2.connect(
+      dbname=dbName,
+      user="postgres",
+      password=os.getenv("DB_PASSWORD"),
+      host="localhost",
+      port="5432"
+    )
+    conn.autocommit = True
+
+    cur = conn.cursor()
+    csvFile = 'tsla_2014_2023.csv'
+
+    # Open the CSV file and read data
+    with open(csvFile, 'r') as file:
+      reader = csv.reader(file)
+      next(reader)  # Skip header row
+      for row in reader:
+        # Insert data into the table
+        cur.execute("""
+          INSERT INTO stock_data
+          (date, open, high, low, close, volume, rsi_7, rsi_14, cci_7, cci_14, sma_50, ema_50, sma_100, ema_100, macd, bollinger, TrueRange, atr_7, atr_14, next_day_close)
+          VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, row)
+    print("Data loaded successfully!")
+  except OperationalError as e:
+    print(f"Error: {e}")
+  finally:
+    cur.close()
+    conn.close()
 
 # createDB("test_db")
-createTable("test_db")
+# createTable("test_db")
+# loadCSVdata("test_db")
