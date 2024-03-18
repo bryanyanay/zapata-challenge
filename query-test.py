388dcb2fee69c416ffb20952584d26201b10fe44
Bryan Yan <74737261+bryanyanay@users.noreply.github.com>
2024-03-18 01:48:57 -0400

    Improve information printed by the test query client

--- query-test.py
+++ query-test.py
@@ -1,19 +1,28 @@
-import requests
 import json
+import requests
+from tabulate import tabulate
 
 url = "http://localhost:8000/query"  
 
 question = {
   "question": "What are the 5 dates when the stock had highest close price?"
 }
-
 reqJSON = json.dumps(question)
-
 headers = {"Content-Type": "application/json"}
 
+print(">>> ASKING: ", question["question"])
 response = requests.post(url, data=reqJSON, headers=headers)
 
 if response.status_code == 200:
-  print(response.json())
+  resJSON = response.json()
+  if (resJSON["ran_successfully"]):
+    print(">>> QUERY SUCCESSFUL")
+    print(tabulate(resJSON["data"], headers=resJSON["columns"]))
+  else:
+    print(">>> QUERY NOT SUCCESSFUL")
+    print(">>> ATTEMPTED QUERY:")
+    print(resJSON["attempted_query"])
+    print(">>> FULL INFO:")
+    print(resJSON["full_info"])
 else:
   print(f"Error: {response.status_code} - {response.text}")
